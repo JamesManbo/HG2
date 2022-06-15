9961ae48ee3a6b59329a82e1cc7a4595d287317a
thangbui123 <buithang.d6dtvt2@gmail.com>
2022-06-16 00:03:49 +0700

    update hrml thử tục hành chính

---
+++ HG.WebApp/Controllers/ThuTucController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HG.WebApp.Controllers
+{
+    public class ThuTucController : Controller
+    {
+        public IActionResult QuanLy()
+        {
+            return View();
+        }
+
+        public IActionResult ThemThuTuc()
+        {
+            return View();
+        }
+    }
+}
