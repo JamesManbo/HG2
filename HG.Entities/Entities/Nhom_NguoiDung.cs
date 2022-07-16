0669fc7fa0365a84ed1dd029036336170a543c40
JamesManbo <anhtuan02399@gmail.com>
2022-07-16 16:03:59 +0700

    cmt nhom_nguoidung

---
+++ HG.Entities/Entities/Nhom_NguoiDung.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Entities.Entities
+{
+    public class Nhom_NguoiDung 
+    {
+        public Guid Id { get; set; }
+        public Guid ma_nguoi_dung { get; set; }
+        public string ma_nhom { get; set; }
+    
+    }
+}
