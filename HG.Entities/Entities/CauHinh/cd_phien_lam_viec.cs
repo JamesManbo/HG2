118345a816090e3129ca86e7b1e172b8d89fa29b
thangbui123 <buithang.d6dtvt2@gmail.com>
2022-07-27 00:21:50 +0700

    cấu hình hệ thống

---
+++ HG.Entities/Entities/CauHinh/cd_phien_lam_viec.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Entities.Entities.CauHinh
+{
+    public class cd_phien_lam_viec : BaseDanhMucModel
+    {
+        //[Key]
+        public int id { get; set; }
+        public string? don_vi { get; set; }
+        public int? so_ban { get; set; }
+        public int? so_phien { get; set; }
+        public int? phien_hien_tai { get; set; }
+
+        [NotMapped]
+        public bool isShow { get; set; }
+    }
+}
