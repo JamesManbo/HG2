995c13a5d3e3acacf81ab4b388dc38bbf42f1736
JamesManbo <anhtuan02399@gmail.com>
2022-07-27 00:09:01 +0700

    user nhom

--- HG.Entities/Entities/AspNetUsersModel.cs
+++ HG.Entities/Entities/AspNetUsersModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HG.Entities.Entities.SuperAdmin;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,5 +12,16 @@ namespace HG.Entities
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Password { get; set; }
+    }
+    public class UnitsMenuRolesOfUser
+    {
+        public Dm_Phong_Ban? dm_Phong_Ban { get; set; }
+        public List<Nhom_Vai_Tro>? nhom_Vai_Tro { get; set; }
+        public List<Asp_vaitro_quyen>? Vai_Tro_Menu_Quyen { get; set; }
+    }
+    public class Nhom_Vai_Tro
+    {
+        public string? ma_nhom { get; set; }
+        public string? ma_vai_tro { get; set; }
     }
 }
