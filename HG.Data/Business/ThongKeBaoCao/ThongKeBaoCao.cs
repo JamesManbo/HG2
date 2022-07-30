0ac0bd3d857e1807aa37dfcf8357ff163e5577f0
ngocnguyenduy <nguyenduyngoc649@gmail.com>
2022-07-31 00:08:49 +0700

    Thong ke bao cao: bao cao so luong ho so

---
+++ HG.Data/Business/ThongKeBaoCao/ThongKeBaoCao.cs
@@ -0,0 +1,45 @@
+﻿using HG.Data.SqlService;
+using HG.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Data.Business.ThongKeBaoCao
+{
+    public class ThongKeBaoCao : BaseDAL
+    {
+        public ThongKeBaoCao(SqlDbProvider dbProvider) : base(dbProvider)
+        {
+
+        }
+
+        public List<SoLuongHoSoModel> BaoCaoSoLuongHoSo(int nam, int quy) {
+            try
+            {
+                List<SoLuongHoSoModel> soLuongHS = new List<SoLuongHoSoModel>();
+                DbProvider.SetCommandText2("bao_cao_so_luong_ho_so", CommandType.StoredProcedure);
+
+                // Input params
+                DbProvider.AddParameter("@nam", nam, SqlDbType.NVarChar);
+                DbProvider.AddParameter("@quy", quy, SqlDbType.NVarChar);
+
+                // Output params
+                DbProvider.AddParameter("ErrCode", DBNull.Value, SqlDbType.Int, 100, ParameterDirection.Output);
+                soLuongHS = DbProvider.ExecuteListObject<SoLuongHoSoModel>();
+                var maloi = Convert.ToInt32(DbProvider.Command.Parameters["ErrCode"].Value.ToString());
+                if (maloi == 1)
+                {
+                    return new List<SoLuongHoSoModel>();
+                }
+                return soLuongHS;
+            }
+            catch (Exception e)
+            {
+                return new List<SoLuongHoSoModel>();
+            }
+        }
+    }
+}
---
+++ HG.Entities/Entities/SoLuongHoSoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Entities
+{
+    public class SoLuongHoSoModel
+    {
+        public int tong_hs_nhan_giai_quyet { get; set; }
+        public int so_ky_truoc_chuyen_qua { get; set; }
+        public int so_moi_tiep_nhan { get; set; }
+        public int tong_hs_da_giai_quyet { get; set; }
+        public int tra_dung_han { get; set; }
+        public int tra_som_han { get; set; }
+        public int tra_qua_han { get; set; }
+        public int tong_hs_dang_giai_quyet { get; set; }
+        public int chua_den_han { get; set; }
+        public int qua_han { get; set; }
+    }
+}
