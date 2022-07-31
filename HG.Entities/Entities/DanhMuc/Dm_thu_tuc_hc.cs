4510d477bac933973acb96ffd51da268ded9ac04
QuangTV <quangtv@gmail.com>
2022-08-01 01:03:58 +0700

    xu ly ho so

---
+++ HG.Entities/Entities/DanhMuc/Dm_thu_tuc_hc.cs
@@ -0,0 +1,24 @@
+﻿using HG.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Entities
+{
+    [Table("Dm_thu_tuc_hc")]
+    public class Dm_thu_tuc_hc : BaseDanhMucModel
+    {
+
+        
+        public string ma_thu_tuc { get; set; }
+        public string ten_thu_tuc { get; set; }
+       
+
+    }
+
+   
+}
