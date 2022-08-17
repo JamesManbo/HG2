47cf76bf36b9cf0dc7dab41008e8b5d4ac804e98
tuấn anh <anhtuan02399@gmail.com>
2022-08-17 17:11:24 +0700

    bao cao full

---
+++ HG.Data/Business/BaoCaoThongKe/BaoCaoThongKeDao.cs
@@ -0,0 +1,46 @@
+﻿using HG.Data.SqlService;
+using HG.Entities;
+using HG.Entities.BaoCaoThongKe;
+using HG.Entities.Entities.DanhMuc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Data.Business.DanhMuc
+{
+    public class BaoCaoThongKeDao : BaseDAL
+    {
+        public BaoCaoThongKeDao(SqlDbProvider dbProvider) : base(dbProvider)
+        {
+
+        }
+
+       
+
+
+        public List<BaoCaoSoLuong> BaoCaoSoLuongHoSo(int nam, int quy, bool chonngay, DateTime tuNgay, DateTime denNgay)
+        {
+            try
+            {
+                DbProvider.SetCommandText2("usp_hoso_baocao_soluong", CommandType.StoredProcedure);
+                DbProvider.AddParameter("nam", nam, SqlDbType.Int);
+                DbProvider.AddParameter("quy", quy, SqlDbType.Int);
+                DbProvider.AddParameter("chonngay", (chonngay ? 1 : 0), SqlDbType.Bit);
+                DbProvider.AddParameter("tuNgayInt", tuNgay.ToString("yyyyMMdd"), SqlDbType.BigInt);
+                DbProvider.AddParameter("denNgayInt", denNgay.ToString("yyyyMMdd"), SqlDbType.BigInt);
+
+                // Lấy về danh sách các trường học
+                var obj = DbProvider.ExecuteListObject<BaoCaoSoLuong>();
+                return obj;
+            }
+            catch (Exception ex)
+            {
+                return new List<BaoCaoSoLuong>();
+            }
+        }
+
+    }
+}
---
+++ HG.WebApp/Models/BaoCaoThongKe/BaoCaoSoLuongRequestModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HG.WebApp.Models.BaoCaoThongKe
+{
+    public class BaoCaoSoLuongRequestModel
+    {
+        public int Nam { get; set; } = DateTime.Now.Year;
+        public int Quy { get; set; } = DateTime.Now.Month;
+        public bool ChonNgay { get; set; } = false;
+
+        public string TuNgayStr { get; set; }
+        public string DenNgayStr { get; set; }
+        public DateTime TuNgay { get; set; } = DateTime.Now;
+        public DateTime DenNgay { get; set; } = DateTime.Now;
+
+
+    }
+}
