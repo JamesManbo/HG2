33a35443e4b7f2fefefed8e2f484023c3b9ff218
TaTuan02399 <anhtuan02399@gmail.com>
2022-09-22 17:02:15 +0700

    cmt

---
+++ HG.Entities/Entities/HoSo/ThongTinTraKQModels.cs
@@ -0,0 +1,22 @@
+﻿using HG.Entities.Entities.DanhMuc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Entities
+{
+    public class ThongTinTraKQModels
+    {
+        public int? ma_ho_so { get; set; }
+        public string? ten_nguoi_nhan { get; set; }
+        public string? so_chung_thuc { get; set; }
+        public string? so_dien_thoai { get; set; }
+        public string? dia_chi { get; set; }
+        public string? le_phi { get; set; }
+        public int? da_thu_phi { get; set; }
+        public string? y_kien { get; set; }
+        public string? file_dinh_kem { get; set; }
+    }
+}
